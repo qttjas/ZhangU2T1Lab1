d90faab12fb8691ac2d8e0379ea5cf2eb3ab99ad
BT_1N3_07 <sample@bths.edu>
2023-09-28 10:53:00 -0400

    commit1

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,5 +1,6 @@
 public class RectangleRunner {
     public static void main(String[] args) {
+
         Rectangle plot1 = new Rectangle(150,200);
         Rectangle plot2 = new Rectangle(125);
         Rectangle plot3 = new Rectangle();
