a75a709dfcfeeafc1d47a0f41214d00b940d797d
Jasmine <jzbrooklynn@gmail.com>
2023-09-28 22:27:18 -0400

    finished

--- src/Point.java
+++ src/Point.java
@@ -7,4 +7,40 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public Point(int pointSame) {
+        x = pointSame;
+        y = pointSame;
+    }
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public int getX() {return x; }
+    public void setX(int newX) { x = newX; }
+
+    public int getY() {return y; }
+    public void setY(int newY) { y = newY; }
+
+    public String coordinate() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public String quadrant() {
+        if (x > 0 && y > 0) {
+            return "I";
+        } else if (x < 0 && y > 0) {
+            return "II";
+        } else if (x < 0 && y < 0 ) {
+            return "III";
+        } else if (x > 0 && y < 0 ) {
+            return "IV";
+        } else if ( x == 0 && y == 0) {
+            return "origin";
+        } else {
+            return "on an axis";
+        }
+    }
 }
